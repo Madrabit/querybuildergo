59861f14d62913a9d5071f0941ef3b33bdf8d73e
Madrabit <madrabot@gmail.com>
2025-06-17 18:55:08 +0300

    рефакторинг product домена fix 2 handler

--- tests/product_repository_test.go
+++ tests/product_repository_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"fmt"
+	"github.com/jmoiron/sqlx"
 	"github.com/stretchr/testify/require"
 	"log"
 	"querybuilder/internal/config"
@@ -13,22 +14,24 @@ import (
 func TestGetAllProducts(t *testing.T) {
 	cnf, err := config.Load()
 	require.NoError(t, err)
-
 	db, err := database.NewMssqlStorage(cnf.DB)
 	require.NoError(t, err)
-	defer db.Close()
-	// Начинаем транзакцию
-
+	defer func(db *sqlx.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal("error closing db")
+		}
+	}(db)
 	require.NoError(t, err)
-	// Откатим транзакцию в конце, чтобы не оставлять изменения
 	store := product.NewStore(db)
 	tx, err := store.BeginTransaction()
+	require.NoError(t, err)
 	products, err := store.GetAllProductsTx(tx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	require.Greater(t, len(products), 0)
-	for _, product := range products {
-		fmt.Println(product)
+	for _, p := range products {
+		fmt.Println(p)
 	}
 }
